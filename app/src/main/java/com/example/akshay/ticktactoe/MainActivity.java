d32209b721c5a706946bc0f71b609a9a455c64f2
QuickChangeSpidey <n.akshay.pandey@gmail.com>
2018-06-01 01:02:52 -0500

    menu added and UI changes on main page

--- app/src/main/java/com/example/akshay/ticktactoe/MainActivity.java
+++ app/src/main/java/com/example/akshay/ticktactoe/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.akshay.ticktactoe;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView =
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_history:
+                                Log.d("test","test1");
+                                break;
+                            case R.id.action_settings:
+                                Log.d("test","test2");
+                                break;
+                            case R.id.action_rules:
+                                Log.d("test","test3");
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+
+
+
+    public void startGame(View view) {
     }
 }
